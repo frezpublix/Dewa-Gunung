e48bfa337baef15d486886b9d02a97a5ef665e9f
Okane <31785499+frezpublix@users.noreply.github.com>
2025-10-07 23:50:14 +0700

    Update MultiTool.lua

--- MultiTool.lua
+++ MultiTool.lua
@@ -1,1 +1,9 @@
 loadstring(game:HttpGet("https://pastefy.app/YhS4CMJk/raw"))()
+-----------------
+-- 🔹 FITUR 🔹 --
+-----------------
+-- Infinite Jump (spasi lebih dari 1x untuk lihat efeknya)
+-- Click Teleport (L-CTRL + Klik Kiri di mouse untuk teleport ke tempat yang ditunjuk oleh Kursor)
+-- Fly Hack (Control dengan WASD + Mouse Shift Lock / Klik Kanan, kecepatan terbang bisa di adjust)
+-- Walk Speed Controller (Bisa mengubah kecepatan Walk Speed sesuka hati, apabila ingin menggunakan Walk Speed jangan pakai Coil/Sprint)
+-- FITUR BARU AKAN SEGERA HADIR
