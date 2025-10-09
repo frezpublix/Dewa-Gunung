c1f4c4e8d1f5bb06167c8709fd90179b16af78de
Fanbez <31785499+fbzpbx@users.noreply.github.com>
2025-10-09 17:50:28 +0700

    Update MultiTools.lua

--- MultiTools.lua
+++ MultiTools.lua
@@ -1,13 +1,17 @@
 loadstring(game:HttpGet("https://pastefy.app/YhS4CMJk/raw"))()
 
--- 🔹 FITUR
--- Infinite Jump (spasi lebih dari 1x untuk lihat efeknya)
--- Click Teleport (L-CTRL + Klik Kiri di mouse untuk teleport ke tempat yang ditunjuk oleh Kursor)
--- Fly Hack (Control dengan WASD + Mouse Shift Lock / Klik Kanan, kecepatan terbang bisa di adjust)
--- Walk Speed Controller (Bisa mengubah kecepatan Walk Speed sesuka hati, apabila ingin menggunakan Walk Speed jangan pakai Coil/Sprint)
--- FITUR BARU AKAN SEGERA HADIR
+--[[ 🔹 FITUR
+> Infinite Jump (spasi lebih dari 1x untuk lihat efeknya)
+> Click Teleport (L-CTRL + Klik Kiri di mouse untuk teleport ke tempat yang ditunjuk oleh Kursor)
+> Fly Hack (Control dengan WASD + Mouse Shift Lock / Klik Kanan, kecepatan terbang bisa di adjust)
+> Walk Speed Controller (Bisa mengubah kecepatan Walk Speed sesuka hati, apabila ingin menggunakan Walk Speed jangan pakai Coil/Sprint)
 
--- 🔹 CHANGELOGS v1.1
--- Menambahkan fitur Minimize Tombol (K)
+🔹 CHANGELOGS v1.1
+1. Menambahkan fitur Minimize Tombol (K)
 
--- Made with ❤️ by Fanbez
+🔹 CHANGELOGS v1.3
+1. Menambahkan efek welcome musik ketika inject menu
+2. Menambahkan tombol EXIT MENU yang akan menonaktifkan semua script secara menyeluruh (bukan minimize)
+
+Made with ❤️ by Fanbez
+]]
